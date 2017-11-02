7935ca05b5858c90e04454b1cee1bf5c5d216876
Abhishekmamidi123 <abhishek.m15@iiits.in>
2017-11-02 11:24:09 +0530

    Authentication with Github with minor errors

---
+++ socialAuth/authentication/views.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AdminPasswordChangeForm, PasswordChangeForm, UserCreationForm
+from django.contrib.auth import update_session_auth_hash, login, authenticate
+from django.contrib import messages
+from django.shortcuts import render, redirect
+
+from social_django.models import UserSocialAuth
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = authenticate(
+                username=form.cleaned_data.get('username'),
+                password=form.cleaned_data.get('password1')
+            )
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+# Create your views here.
+@login_required
+def home(request):
+    return render(request, 'authentication/home.html')
